8a44c818e78b33ca30d14782f866119056eef11a
KrzychuJedi <tharealiestjedi@gmail.com>
2019-02-15 21:13:07 +0100

    read name from console input

--- src/main/java/com/sda/project/Main.java
+++ src/main/java/com/sda/project/Main.java
@@ -1,8 +1,18 @@
 package com.sda.project;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me your name!");
+        String name = null;
+        if(scanner.hasNext()){
+            name = scanner.next();
+        }
+
+        System.out.println("Hello " + name);
     }
 }
